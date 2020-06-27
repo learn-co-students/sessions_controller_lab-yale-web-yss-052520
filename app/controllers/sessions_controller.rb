1da412370de19d90139f2815133a9bf219471d0d
ZIming Mao <ziming.mao@yale.edu>
2020-06-27 09:57:18 -0400

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,25 @@
+class SessionsController < ApplicationController
+
+
+    def new
+
+    end
+
+
+    def create
+        if params[:name].nil? || params[:name] == ''
+            redirect_to '/login'
+        else
+            session[:name] = params[:name]
+            redirect_to '/'
+        end
+    end
+
+
+    def destroy
+        # byebug
+        session.delete :name
+        redirect_to '/'
+    end
+
+end
